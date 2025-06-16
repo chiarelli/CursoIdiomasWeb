887e2c073b10562dc1ad6c10d4e57d5591d5e846
Raphael Mathias <oi.sucesso@gmail.com>
2025-06-16 21:33:32 +0000

    Enabled "provideAnimationsAsync"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -33,6 +33,7 @@ export const appConfig: ApplicationConfig = {
     IconSetService,
     provideAnimationsAsync(),
     provideHttpClient(),
-    provideNgxMask()
+    provideNgxMask(),
+    provideAnimationsAsync()
   ]
 };
