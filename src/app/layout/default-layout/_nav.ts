8e9f4603a6b6b895525d7bfa8ce57a327f1bf5fe
Raphael Mathias <oi.sucesso@gmail.com>
2025-06-18 04:42:08 +0000

    Default message when there are no records in the student list

--- src/app/layout/default-layout/_nav.ts
+++ src/app/layout/default-layout/_nav.ts
@@ -30,7 +30,7 @@ export const navItems: INavData[] = [
   {
     name: 'Turmas',
     url: '/turmas',
-    iconComponent: { name: 'cil-book' },
+    iconComponent: { name: 'cil-star' },
     children: [
       {
         name: 'Listar',
