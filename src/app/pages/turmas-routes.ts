e8052c1c52e27ced93edf40017610ab2e78e3a22
Raphael Mathias <oi.sucesso@gmail.com>
2025-06-18 01:27:57 +0000

    Page titles

--- src/app/pages/turmas-routes.ts
+++ src/app/pages/turmas-routes.ts
@@ -3,15 +3,24 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'listagem',
-    loadComponent: () => import('./listar-turmas/listar-turmas.component').then(m => m.ListarTurmasComponent)
+    loadComponent: () => import('./listar-turmas/listar-turmas.component').then(m => m.ListarTurmasComponent),
+    data: {
+      title: 'Listagem de Turmas'
+    }
   },
   {
     path: 'cadastrar',
-    loadComponent: () => import('./cadastrar-turma/cadastrar-turma.component').then(m => m.CadastrarTurmaComponent)
+    loadComponent: () => import('./cadastrar-turma/cadastrar-turma.component').then(m => m.CadastrarTurmaComponent),
+    data: {
+      title: 'Cadastrar Turma'
+    }
   },
   {
     path: 'editar/:id',
-    loadComponent: () => import('./editar-turma/editar-turma.component').then(m => m.EditarTurmaComponent)
+    loadComponent: () => import('./editar-turma/editar-turma.component').then(m => m.EditarTurmaComponent),
+    data: {
+      title: 'Editar Turma'
+    }
   },
   {
     path: 'aluno/:aluno_id/matriculado',
